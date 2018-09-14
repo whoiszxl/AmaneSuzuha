73ffd82ee9f9dd8c67c3d3d8bafcfe5a0f6711a1
zhouxiaolong <whoiszxl@gmail.com>
2018-09-14 13:40:15 +0800

    coinbase创世区块的未花费交易输出模型

---
+++ golang_blockchain/code/part7-transaction-UTXO/BLC/CLI.go
@@ -0,0 +1,120 @@
+package BLC
+
+import (
+	"fmt"
+	"os"
+	"flag"
+	"log"
+)
+
+type CLI struct {}
+
+//使用说明
+func printUsage()  {
+	fmt.Println("Usage:")
+	fmt.Println("1. createblockchain -data -- 交易数据.")
+	fmt.Println("2. addblock -data DATA -- 交易数据.")
+	fmt.Println("3. printchain -- 输出区块信息.")
+}
+
+//判断是否有效
+func isValidArgs()  {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+//添加一个区块
+func (cli *CLI) addBlock(txs []*Transaction)  {
+	if dbExists() == false {
+		fmt.Println("数据不存在.......")
+		os.Exit(1)
+	}
+
+	blockchain := BlockchainObject()
+	defer blockchain.DB.Close()
+	blockchain.AddBlockToBlockchain(txs)
+}
+
+//打印所有区块
+func (cli *CLI) printchain()  {
+	if dbExists() == false {
+		fmt.Println("数据不存在.......")
+		os.Exit(1)
+	}
+
+	blockchain := BlockchainObject()
+	defer blockchain.DB.Close()
+	blockchain.PrintChain()
+}
+
+//创建创世区块链
+func (cli *CLI) createGenesisBlockchain(data string)  {
+
+	CreateBlockchainWithGenesisBlock(data)
+}
+
+//cli的运行方法
+func (cli *CLI) Run()  {
+
+	//判断是否有效
+	isValidArgs()
+
+	//添加命令
+	addBlockCmd := flag.NewFlagSet("addblock",flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain",flag.ExitOnError)
+	createBlockchainCmd := flag.NewFlagSet("createblockchain",flag.ExitOnError)
+
+	//给命令添加data参数,并接收保存
+	flagAddBlockData := addBlockCmd.String("data","http://whoiszxl.com","交易数据......")
+	flagCreateBlockchainWithData := createBlockchainCmd.String("data","Genesis block data......","创世区块交易数据......")
+
+	switch os.Args[1] {
+		case "addblock"://第一个参数为addblock
+			err := addBlockCmd.Parse(os.Args[2:])
+			if err != nil {
+				log.Panic(err)
+			}
+		case "printchain"://第一个参数为printchain
+			err := printChainCmd.Parse(os.Args[2:])
+			if err != nil {
+				log.Panic(err)
+			}
+		case "createblockchain":
+			err := createBlockchainCmd.Parse(os.Args[2:])
+			if err != nil {
+				log.Panic(err)
+			}
+		default:
+			printUsage()
+			os.Exit(1)
+	}
+
+	//如果-data为空，打印输出使用说明
+	if addBlockCmd.Parsed() {
+		if *flagAddBlockData == "" {
+			printUsage()
+			os.Exit(1)
+		}
+		//不然则调用添加区块的方法
+		cli.addBlock([]*Transaction{})
+	}
+
+	//printchain命令，输出所有区块
+	if printChainCmd.Parsed() {
+		cli.printchain()
+	}
+
+	//createblockchain命令，创建带创世区块的区块链
+	if createBlockchainCmd.Parsed() {
+
+		if *flagCreateBlockchainWithData == "" {
+			fmt.Println("交易数据不能为空......")
+			printUsage()
+			os.Exit(1)
+		}
+
+		cli.createGenesisBlockchain(*flagCreateBlockchainWithData)
+	}
+}
---
+++ golang_blockchain/code/part7-transaction-UTXO/BLC/Transaction.go
@@ -0,0 +1,56 @@
+package BLC
+
+import (
+	"bytes"
+	"log"
+	"encoding/gob"
+	"crypto/sha256"
+)
+
+// UTXO
+type Transaction struct {
+
+	//1. 交易hash
+	TxHash []byte
+
+	//2. 输入
+	Vins []*TXInput
+
+	//3. 输出
+	Vouts []*TXOutput
+}
+
+
+//1. 创世区块创建时的Transaction
+func NewCoinbaseTransaction(address string) *Transaction {
+
+	//代表消费
+	txInput := &TXInput{[]byte{}, -1, "Genesis Data"}
+
+	//代表收入
+	txOutput := &TXOutput{10, address}
+
+	//封装coinbase
+	txCoinbase := &Transaction{ []byte{}, []*TXInput{txInput}, []*TXOutput{txOutput}}
+
+	//设置hash值
+	txCoinbase.HashTransaction()
+	
+	return txCoinbase
+}
+
+//将Transaction中的tx转为hash值
+func (tx *Transaction) HashTransaction() {
+	
+	var result bytes.Buffer
+
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	hash := sha256.Sum256(result.Bytes())
+	tx.TxHash = hash[:]
+}
